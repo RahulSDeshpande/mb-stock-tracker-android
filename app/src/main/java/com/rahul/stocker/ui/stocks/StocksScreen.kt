685bc1c90e5288acc992b36d0bb378e8afca9a3b
Rahul Deshpande <rahul.deshpande92@gmail.com>
2025-08-12 19:33:59 +0400

    fix: :lipstick: Add divider to StockRow

--- app/src/main/java/com/rahul/stocker/ui/stocks/StocksScreen.kt
+++ app/src/main/java/com/rahul/stocker/ui/stocks/StocksScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -65,6 +66,7 @@ fun StocksScreen(
         ) {
             items(items = viewState.stocks) { stock ->
                 StockRow(stock = stock)
+                HorizontalDivider(thickness = 0.5.dp)
             }
         }
     }
