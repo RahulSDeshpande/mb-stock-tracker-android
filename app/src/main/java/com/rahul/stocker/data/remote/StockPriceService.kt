cd450205033a5f7d94260cceeecb5c3d4795ee9f
Rahul Deshpande <rahul.deshpande92@gmail.com>
2025-08-12 18:27:10 +0400

    refactor: :recycle: :bug: Refine & update interface structures [WIP]

--- app/src/main/java/com/rahul/stocker/data/remote/StockPriceService.kt
+++ app/src/main/java/com/rahul/stocker/data/remote/StockPriceService.kt
@@ -2,8 +2,11 @@ package com.rahul.stocker.data.remote
 
 import com.rahul.stocker.domain.model.StockPriceEventModel
 import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
 
 interface StockPriceService {
+    val isConnected: StateFlow<Boolean>
+
     fun connect()
 
     fun disconnect()
