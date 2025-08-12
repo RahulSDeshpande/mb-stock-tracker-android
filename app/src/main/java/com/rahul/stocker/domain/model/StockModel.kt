041421a68aaf8e323568846af9429ea76f8d83ed
Rahul Deshpande <rahul.deshpande92@gmail.com>
2025-08-12 18:25:39 +0400

    refactor: :label: Update model params

--- app/src/main/java/com/rahul/stocker/domain/model/StockModel.kt
+++ app/src/main/java/com/rahul/stocker/domain/model/StockModel.kt
@@ -3,4 +3,12 @@ package com.rahul.stocker.domain.model
 data class StockModel(
     val symbol: String,
     val price: Double,
-)
+    val previousPrice: Double?,
+    val lastChangedTimestamp: Long?,
+) {
+    val isPriceUp: Boolean
+        get() = previousPrice != null && price > previousPrice
+
+    val isPriceDown: Boolean
+        get() = previousPrice != null && price < previousPrice
+}
