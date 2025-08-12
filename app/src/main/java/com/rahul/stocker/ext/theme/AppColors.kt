6c2a7a707b9923692cd10e1b72a5f9a8e5a3f5ed
Rahul Deshpande <rahul.deshpande92@gmail.com>
2025-08-12 21:25:44 +0400

    feat: :lipstick: Add Google Sans font and app colors [WIP]

---
+++ app/src/main/java/com/rahul/stocker/ext/theme/AppColors.kt
@@ -0,0 +1,13 @@
+package com.rahul.stocker.ext.theme
+
+import androidx.compose.ui.graphics.Color
+
+object AppColors {
+    val priceUpBgColor = Color(0x3324BB59)
+    val priceDownBgColor = Color(0x33D73B37)
+    val priceStillBgColor = Color.Companion.Transparent
+
+    val priceUpArrowColor = Color(0xFF14AD4A)
+    val priceDownArrowColor = Color(0xFFD02D29)
+    val priceStillArrowColor = Color.Companion.Transparent
+}
